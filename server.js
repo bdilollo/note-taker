f071d0aa28158716ac76934822d1c8e913673909
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-10-28 20:45:31 -0500

    get routes added

--- server.js
+++ server.js
@@ -26,13 +26,16 @@ app.get('/api/notes', (req, res) =>
             return;
         }
         let notesJSON = JSON.parse(data);
-        console.log(notesJSON);
-        return notesJSON;
+        res.json(notesJSON);
     })
 );
 
+app.get('*', (req, res) =>
+res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
 app.post('/api/notes', (req, res) => 
-    console.log(res)
+
 );
     
 
